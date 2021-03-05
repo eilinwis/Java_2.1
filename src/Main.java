d6dc47ab417c23b06da1a817e21db09a59f29658
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-03-05 21:21:28 +0300

    Report added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 1_647_500_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
+        System.out.println(total);
+    }
+}
