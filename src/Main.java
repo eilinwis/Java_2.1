e733faa6c8ad616390460576fe8fd24901ea6c03
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-03-05 21:23:15 +0300

    App edited

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 1_647_500_000;
+        int balance = 2_000_000_000;
         int transfer = 500_000_000;
         int total = balance + transfer;
         System.out.println(total);
